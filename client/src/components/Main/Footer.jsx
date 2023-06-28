aaf36d144e5329cbb77ee3538d34cafb353c9307
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2023-06-28 22:48:10 +0400

    footer-change-datas-from-backend

--- client/src/components/Main/Footer.jsx
+++ client/src/components/Main/Footer.jsx
@@ -1,5 +1,5 @@
 import { Grid } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Movflix from '../../assets/images/logo.png'
 
 import FacebookIcon from '@mui/icons-material/Facebook';
@@ -12,7 +12,47 @@ import { Link } from 'react-router-dom'
 import EditLocationIcon from '@mui/icons-material/EditLocation';
 import HeadphonesIcon from '@mui/icons-material/Headphones';
 import DraftsIcon from '@mui/icons-material/Drafts';
+import { getAllBlogs, getAllContacts, getAllFilms } from '../../api/requests';
+import { Box } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 const Footer = () => {
+    const [films, setFilms] = useState([])
+    const [allFilms, setAllFilms] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [blogs, setBlogs] = useState([]);
+    const [contacts, setContacts] = useState([])
+
+    useEffect(() => {
+        getAllContacts().then((res) => {
+            setContacts(res.data)
+        })
+    }, [])
+    useEffect(() => {
+        getAllBlogs().then((res) => {
+            setBlogs(res.data);
+        });
+    }, []);
+
+    useEffect(() => {
+        getAllFilms().then((res) => {
+            const formattedFilms = res.data.filter((film) => {
+                const releaseDate = new Date(film.releaseDate);
+                const currentDate = new Date();
+                return releaseDate > currentDate;
+            });
+            setFilms(formattedFilms);
+            setAllFilms(formattedFilms);
+
+            const uniqueCategories = [...new Set(formattedFilms.map((film) => film.category))];
+            setCategories(uniqueCategories);
+        });
+    }, []);
+
+    const lastBlogs = blogs.slice(-2);
     return (
         <footer style={{ width: '100%' }}>
 
@@ -21,26 +61,53 @@ const Footer = () => {
                     <Grid item xs={12} sm={6} md={3}>
                         <ul style={{ color: 'white', listStyle: 'none', lineHeight: '2em' }}>
                             <li><img width={200} src={Movflix} alt='logo' /></li>
-                            <li>Movflx Online the relase etras thats sheets continig passag.</li>
-                            <li> <EditLocationIcon /> Address : PO Box W75 Street</li>
-                            <li><HeadphonesIcon />  Phone : +24 1245 654 235</li>
-                            <li><DraftsIcon />  Email : info@exemple.com</li>
+                            {contacts && contacts.map((contact)=>{
+                                return (
+                                    <>
+                                    <li>{contact.desc}</li>
+                                    <li> <EditLocationIcon /> Address : {contact.address}</li>
+                                    <li><HeadphonesIcon />  Phone : {contact.phone}</li>
+                                    <li><DraftsIcon />  Email : {contact.email}</li>
+                                    </>
+                                )
+                            })}
                         </ul>
                     </Grid>
 
                     <Grid item xs={12} sm={6} md={3}>
                         <ul style={{ color: 'white', listStyle: 'none', lineHeight: '2.5em' }}>
                             <li><h2>Categories</h2></li>
-                            <li>Action Movies</li>
-                            <li>Horror</li>
-                            <li>Movies</li>
-                            <li>Streaming</li>
+                            {categories.map((category) => (
+                                <li key={category}>{category}</li>
+                            ))}
                         </ul>
                     </Grid>
 
                     <Grid item xs={12} sm={6} md={3}>
                         <ul style={{ color: 'white', listStyle: 'none', lineHeight: '2.5em' }}>
                             <li><h2>Recent Posts</h2></li>
+                            {lastBlogs && lastBlogs.map((last) => {
+                                return (
+                                    <Card style={{ backgroundColor: 'transparent' }} sx={{ maxWidth: 345, display: 'flex' }} >
+                                        <CardMedia
+                                            component="img"
+                                            sx={{ width: 151 }}
+                                            image={last.image}
+                                            alt="Live from space album cover"
+                                        />
+                                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                            <CardContent sx={{ flex: '1 0 auto' }} style={{ color: 'white' }}>
+                                                <Typography component="div" variant="h5">
+                                                    {last.title}
+                                                </Typography>
+                                                <Typography style={{ color: 'white', display: 'flex', alignItems: 'center' }} variant="subtitle1" color="text.secondary" component="div">
+                                                    <AccessTimeIcon style={{ color: 'yellow' }} /> {last.releaseDate}
+                                                </Typography>
+                                            </CardContent>
+                                        </Box>
+                                    </Card>
+                                )
+                            })}
                         </ul>
                     </Grid>
 
@@ -62,10 +129,10 @@ const Footer = () => {
                 </Grid>
             </div>
 
-            <div style={{  padding: '15px 40px', backgroundColor: '#222' }}>
+            <div style={{ padding: '15px 40px', backgroundColor: '#222' }}>
                 <Grid container spacing={2}>
                     <Grid item xs={12} sm={6} md={6} style={{ display: 'flex', alignItems: 'center' }}>
-                        <p style={{ color: 'gray',margin: 0 }}>Copyright © 2022 All Rights Reserved By Movflx</p>
+                        <p style={{ color: 'gray', margin: 0 }}>Copyright © 2022 All Rights Reserved By Movflx</p>
                     </Grid>
 
                     <Grid item xs={12} sm={6} md={6} style={{ display: 'flex', justifyContent: 'flex-end' }}>
