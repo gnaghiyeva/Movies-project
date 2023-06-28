9ba8899a37f7b12b425498891026b1f1eab7baa4
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2023-06-28 14:42:51 +0400

    slider-responsive

--- client/src/components/Main/Footer.jsx
+++ client/src/components/Main/Footer.jsx
@@ -14,62 +14,63 @@ import HeadphonesIcon from '@mui/icons-material/Headphones';
 import DraftsIcon from '@mui/icons-material/Drafts';
 const Footer = () => {
     return (
-        <footer>
+        // <footer>
 
-            <div className={footerHome.footer} style={{padding:'30px 50px'}}>
-                <Grid container spacing={2}>
-                    <Grid item xs={6} md={3}>
-                        <ul style={{ color: 'white', listStyle: 'none', lineHeight:'2em' }}>
-                            <li><img width={200} src={Movflix} alt='logo' /></li>
-                            <li>Movflx Online the relase etras thats sheets continig passag.</li>
-                            <li> <EditLocationIcon/> Address : PO Box W75 Street</li>
-                            <li><HeadphonesIcon/>  Phone : +24 1245 654 235</li>
-                            <li><DraftsIcon/>  Email : info@exemple.com</li>
-                        </ul>
-                    </Grid>
+        //     <div className={footerHome.footer} style={{padding:'30px 50px'}}>
+        //         <Grid container spacing={2}>
+        //             <Grid item xs={6} md={3}>
+        //                 <ul style={{ color: 'white', listStyle: 'none', lineHeight:'2em' }}>
+        //                     <li><img width={200} src={Movflix} alt='logo' /></li>
+        //                     <li>Movflx Online the relase etras thats sheets continig passag.</li>
+        //                     <li> <EditLocationIcon/> Address : PO Box W75 Street</li>
+        //                     <li><HeadphonesIcon/>  Phone : +24 1245 654 235</li>
+        //                     <li><DraftsIcon/>  Email : info@exemple.com</li>
+        //                 </ul>
+        //             </Grid>
 
-                    <Grid item xs={6} md={3}>
-                        <ul style={{ color: 'white', listStyle: 'none', lineHeight:'2.5em'  }}>
-                            <li><h2>Categories</h2></li>
-                            <li>Action Movies</li>
-                            <li>Horror</li>
-                            <li>Movies</li>
-                            <li>Streaming</li>
-                        </ul>
-                    </Grid>
+        //             <Grid item xs={6} md={3}>
+        //                 <ul style={{ color: 'white', listStyle: 'none', lineHeight:'2.5em'  }}>
+        //                     <li><h2>Categories</h2></li>
+        //                     <li>Action Movies</li>
+        //                     <li>Horror</li>
+        //                     <li>Movies</li>
+        //                     <li>Streaming</li>
+        //                 </ul>
+        //             </Grid>
 
-                    <Grid item xs={6} md={3}>
-                        <ul style={{ color: 'white', listStyle: 'none', lineHeight:'2.5em'  }}>
-                            <li><h2>Recent Posts</h2></li>
-                        </ul>
-                    </Grid>
+        //             <Grid item xs={6} md={3}>
+        //                 <ul style={{ color: 'white', listStyle: 'none', lineHeight:'2.5em'  }}>
+        //                     <li><h2>Recent Posts</h2></li>
+        //                 </ul>
+        //             </Grid>
 
                     
 
-                    <Grid item xs={6} md={3}>
+        //             <Grid item xs={6} md={3}>
 
-                        <ul style={{ color: 'white', listStyle: 'none' , lineHeight:'2.5em'  }}>
-                            <li><h2>Follow us</h2></li>
-                            <div style={{display:'flex', gap:'15px'}}>
-                            <li><Link to='https://www.facebook.com/ColorsTV/'><FacebookIcon style={{ color: 'rgb(59,89,152)', fontSize: '45px' }} /></Link></li>
-                            <li><Link to='https://twitter.com/ColorsTV?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor'><TwitterIcon style={{ color: 'rgb(85,172,238)', fontSize: '45px' }} /></Link></li>
-                            <li><Link to='https://in.pinterest.com/ColorsTv/'><PinterestIcon style={{ color: 'rgb(215,30,24)', fontSize: '45px' }} /></Link></li>
-                            <li><Link to='https://www.linkedin.com/company/colorstv'><LinkedInIcon style={{ color: 'rgb(21,101,192)', fontSize: '45px' }} /></Link></li>
-                            </div>
-                        </ul>
+        //                 <ul style={{ color: 'white', listStyle: 'none' , lineHeight:'2.5em'  }}>
+        //                     <li><h2>Follow us</h2></li>
+        //                     <div style={{display:'flex', gap:'15px'}}>
+        //                     <li><Link to='https://www.facebook.com/ColorsTV/'><FacebookIcon style={{ color: 'rgb(59,89,152)', fontSize: '45px' }} /></Link></li>
+        //                     <li><Link to='https://twitter.com/ColorsTV?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor'><TwitterIcon style={{ color: 'rgb(85,172,238)', fontSize: '45px' }} /></Link></li>
+        //                     <li><Link to='https://in.pinterest.com/ColorsTv/'><PinterestIcon style={{ color: 'rgb(215,30,24)', fontSize: '45px' }} /></Link></li>
+        //                     <li><Link to='https://www.linkedin.com/company/colorstv'><LinkedInIcon style={{ color: 'rgb(21,101,192)', fontSize: '45px' }} /></Link></li>
+        //                     </div>
+        //                 </ul>
 
-                    </Grid>
-                </Grid>
-            </div>
+        //             </Grid>
+        //         </Grid>
+        //     </div>
 
-            <div style={{display:'flex', justifyContent:'space-around', padding:'15px 0', alignItems:'center', backgroundColor:'#222'}}>
-                <p style={{color:'gray'}}>Copyright © 2022 All Rights Reserved By Movflx</p>
+        //     <div style={{display:'flex', justifyContent:'space-around', padding:'15px 0', alignItems:'center', backgroundColor:'#222'}}>
+        //         <p style={{color:'gray'}}>Copyright © 2022 All Rights Reserved By Movflx</p>
 
-                <img src={cardsVisa} alt='cards'/>
-            </div>
+        //         <img src={cardsVisa} alt='cards'/>
+        //     </div>
             
 
-        </footer>
+        // </footer>
+        <></>
     )
 }
 
--- client/src/pages/Main/Home/Sliders/MainSliders.jsx
+++ client/src/pages/Main/Home/Sliders/MainSliders.jsx
@@ -10,32 +10,34 @@ import "../../../../App.css";
 import { Button } from '@mui/material';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import { getAllSliders } from '../../../../api/requests';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import sliderStyle from '../../../../assets/styles/slider.module.css'
+
 const MainSliders = () => {
-    const [sliders,setSliders] = useState([]);
-    useEffect(()=>{
-       getAllSliders().then((res)=>{
-        setSliders(res.data)
-        console.log(res.data)
-       })
-    },[])
+  const [sliders, setSliders] = useState([]);
+  useEffect(() => {
+    getAllSliders().then((res) => {
+      setSliders(res.data)
+      console.log(res.data)
+    })
+  }, [])
   return (
-    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+    <Swiper style={{ width: '100%' }} navigation={true} modules={[Navigation]} className="mySwiper">
       {sliders && sliders.map((slider) => (
-        <SwiperSlide key={slider._id} style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${slider.image})` ,backgroundSize: 'cover', backgroundPosition: 'center'}}>
-          <article style={{textAlign:'left', padding:'300px 40px'}}>
-          <h3 style={{color:'yellow'}}>MOVFLX</h3>
-          <h1 style={{color:'white', fontWeight:'900', fontSize:'60px',lineHeight: '1.2'}}>Unlimited Movie, TVs <br/> Shows, & More.</h1>
-          <Button variant='contained'  style={{backgroundColor:'transparent', border:'1px solid yellow', borderRadius:'25px',color:'white'}}  onMouseEnter={(e) => {
-    e.target.style.backgroundColor = 'yellow';
-    e.target.style.color = 'black';
-  }}
-  onMouseLeave={(e) => {
-    e.target.style.backgroundColor = 'transparent';
-    e.target.style.color = 'white';
-  }}>
-           <PlayArrowIcon/> <Link style={{color:'black'}} to='/movie'>WATCH NOW</Link> 
-          </Button>
+        <SwiperSlide key={slider._id} style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${slider.image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+          <article className={sliderStyle.slider_article}>
+            <h3 className={sliderStyle.slider_article_h3}>MOVFLX</h3>
+            <h1 className={sliderStyle.slider_article_h1}>Unlimited Movie, TVs <br /> Shows, & More.</h1>
+            <Button variant='contained' style={{ backgroundColor: 'transparent', border: '1px solid yellow', borderRadius: '25px', color: 'white' }} onMouseEnter={(e) => {
+              e.target.style.backgroundColor = 'yellow';
+              e.target.style.color = 'black';
+            }}
+              onMouseLeave={(e) => {
+                e.target.style.backgroundColor = 'transparent';
+                e.target.style.color = 'white';
+              }}>
+              <PlayArrowIcon /> <Link style={{ color: 'black' }} to='/movie'>WATCH NOW</Link>
+            </Button>
           </article>
         </SwiperSlide>
       ))}
